51747db31a51603d2093a285959e55221820577d
tada195908 <48470775+tada195908@users.noreply.github.com>
2020-12-15 09:20:14 +0900

    Update InsertionSort.java

--- src/sort/InsertionSort.java
+++ src/sort/InsertionSort.java
@@ -26,33 +26,16 @@ public class InsertionSort<T extends Comparable<T>>
     @Override
     public List<T> doSort() {
         int n = data.size();
-        for (int i = 1; i < n; i++) {
-            int k = findPosition(0, i, i);
-            if (k != i) {
-                insert(i, k);
-            }
-        }
+
+
+
+
+
+
         return data;
     }
 
-    /**
-     * 要素kを挿入する位置を調べるために、要素kよりも小さい要素を前方に向かって探す
-     *
-     * @param from 範囲の下限
-     * @param to 範囲の上限（toは含まれない）
-     * @param k
-     * @return 発見した位置、なかった場合はfromを返す
-     */
-    protected int findPosition(int from, int to, int k) {
-        for (int i = to - 1; i >= from; i--) {
-            if (lesseqByIndex(i, k)) {
-                return i + 1;
-            }
-        }
-        return from;
-    }
-
-    /**
+/**
      * @param args the command line arguments
      * @throws java.io.IOException
      */
